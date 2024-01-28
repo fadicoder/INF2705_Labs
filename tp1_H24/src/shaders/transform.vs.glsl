a0c8731088d87d1345c0afe70e52aae7c235abce
Charles Khoury <charles.khoury@polymtl.ca>
2024-01-28 13:17:45 -0500

    feat: Ajout des shaders de transformation

--- tp1_H24/src/shaders/transform.vs.glsl
+++ tp1_H24/src/shaders/transform.vs.glsl
@@ -1,1 +1,12 @@
 #version 330 core
+
+uniform in mat4 mvp;
+layout(location=0) in vec4 a_position;
+layout(location=1) in vec4 a_color;
+
+out vec4 color;
+
+void main(void){
+    gl_Position = a_position * mvp;
+    color = a_color;
+}
