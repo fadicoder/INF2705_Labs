046c03f565a88a9ee1f4a2748044d0ca68d9592d
Fadi Beirouty <beirouty.fadi@gmail.com>
2024-02-26 17:24:29 -0500

    Skybox is working finally!

--- tp2_H24/src/shaders/skybox.vs.glsl
+++ tp2_H24/src/shaders/skybox.vs.glsl
@@ -10,5 +10,6 @@ uniform mat4 mvp;
 void main(void){
     texCoord = a_position;
     vec4 worldPos = mvp * vec4(a_position, 1.0f);
-    gl_Position = vec4(worldPos.x, worldPos.y, 1.0f, 1.0f);
+    gl_Position = vec4(worldPos.x, worldPos.y, worldPos.w, worldPos.w);
+
 }
