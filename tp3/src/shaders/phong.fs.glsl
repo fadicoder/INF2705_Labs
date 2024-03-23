05f427988b04abf881f2f00e3e21524b243b6056
Fadi Beirouty <beirouty.fadi@gmail.com>
2024-03-23 16:21:57 -0400

    Phong marche avec les 3 spots mais sans le Direct3D

--- tp3/src/shaders/phong.fs.glsl
+++ tp3/src/shaders/phong.fs.glsl
@@ -63,27 +63,16 @@ float calculateSpot(vec3 l, vec3 n, int spotIndex) {
     return spotFacteur;
 }
 
-void main() {
-    // Emission;
-    vec3 specAndDiffuse = vec3(0.0, 0.0, 0.0);
-    vec3 color = mat.emission;
-
-    // Ambiant:
-    color += mat.ambient * lightModelAmbient;
-    color += mat.ambient * lights[0].ambient;
-
-    // calculs nécessaires pour diffuse et spéculaire:
-    vec3 l = normalize(attribIn.lightDir[0]);
-    vec3 n = normalize(gl_FrontFacing ? attribIn.normal : -attribIn.normal);
-    vec3 o = normalize(attribIn.obsPos);
-    float dist = length(attribIn.lightDir[0]);
+vec3 calculateReflexion(vec3 n, vec3 o, int spotIndex) {
+    vec3 l = normalize(attribIn.lightDir[spotIndex]);
+    float dist = length(attribIn.lightDir[spotIndex]);
     float distFactor = min(1.0 / (dist*dist), 1.0);
     float dotProd = dot(n, l);
-
+    vec3 reflexionColor = vec3(0.0, 0.0, 0.0);
     // seulment calucler diffuse et speculaire quand la face est éclairée.
     if (dotProd > 0.0) {
         // Diffuse:
-        specAndDiffuse += (mat.diffuse * lights[0].diffuse * dotProd) * distFactor;
+        reflexionColor += (mat.diffuse * lights[spotIndex].diffuse * dotProd) * distFactor;
 
         // Spectulaire:
         // Calculer l'intensité de la réflection spéculaire selon la formule de Blinn ou Phong. Elle dépend de la position de la lumière, de la normale et de la position de l'observateur.
@@ -92,14 +81,36 @@ void main() {
         // Si le résultat est positif (il y a de la réflexion spéculaire).
         if (specIntensity > 0) {
             float shine = pow(specIntensity, mat.shininess);
-            specAndDiffuse += (shine * mat.specular * lights[0].specular) * distFactor;
+            reflexionColor += (shine * mat.specular * lights[0].specular) * distFactor;
         }
     }
 
     if (useSpotlight) {
-        specAndDiffuse *= calculateSpot(l, n, 0);
+        reflexionColor *= calculateSpot(l, n, spotIndex);
     }
 
-    FragColor = vec4(color + specAndDiffuse, 1.0f);
+    return reflexionColor;
+}
+
+void main() {
+    vec3 n = normalize(gl_FrontFacing ? attribIn.normal : -attribIn.normal);
+    vec3 o = normalize(attribIn.obsPos);
+
+    vec3 emissionColor = mat.emission;
+    vec3 reflexionColor = vec3(0.0, 0.0, 0.0);
+    vec3 ambiantColor = vec3(0.0, 0.0, 0.0);
+
+    // Ambiant:
+    ambiantColor += mat.ambient * lightModelAmbient;
+    ambiantColor += mat.ambient * lights[0].ambient;
+    ambiantColor += mat.ambient * lights[1].ambient;
+    ambiantColor += mat.ambient * lights[2].ambient;
+
+    // diffuse et speculaire
+    reflexionColor += calculateReflexion(n, o, 0);
+    reflexionColor += calculateReflexion(n, o, 1);
+    reflexionColor += calculateReflexion(n, o, 2);
+
+    FragColor = vec4(emissionColor + ambiantColor + reflexionColor, 1.0f);
     FragColor = clamp(FragColor, 0.0, 1.0);
 }
